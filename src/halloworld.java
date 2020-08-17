7a0346b361d9eee1b63b4f995318599e656ba66b
Farhan <s341869@oslomet.no>
2020-08-17 18:43:22 +0200

    Initial commit

---
+++ src/halloworld.java
@@ -0,0 +1,10 @@
+public class halloworld {
+    public static void main(String[] args) {
+        System.out.println("Hallo world");
+        System.out.println("Teller til 10");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
